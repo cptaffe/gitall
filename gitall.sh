487c6d09aa7322b3537c68e92229357af70e8dce
Connor Taffe <cpaynetaffe@gmail.com>
2014-11-03 14:25:41 -0600

    removed sed -r, now cross platform (gnu & bsd)

--- gitall.sh
+++ gitall.sh
@@ -3,7 +3,7 @@
 # save repository names
 GITHUBUSER="$1"
 
-REPONAMES="$(curl -u $GITHUBUSER 'https://api.github.com/users/cptaffe/repos' | grep "\"name\": " | sed -r 's/"name": "(.*)"/\1/' | sed s/,//)"
+REPONAMES="$(curl -u $GITHUBUSER 'https://api.github.com/users/cptaffe/repos' | grep "\"name\": " | sed s/"\"name\": \"\(.*\)\""/"\1"/ | sed s/,//)"
 
 for name in $REPONAMES
 do
