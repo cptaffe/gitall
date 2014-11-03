5903da40b4f3c929cb3aa5e3f050af0935003606
Connor Taffe <cpaynetaffe@gmail.com>
2014-11-02 23:38:55 -0600

    first argument is username

--- gitall.sh
+++ gitall.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # save repository names
+GITHUBUSER="$1"
 
-REPONAMES="$(curl -u cptaffe 'https://api.github.com/users/cptaffe/repos' | grep "\"name\": " | sed -r 's/"name": "(.*)"/\1/' | sed s/,//)"
+REPONAMES="$(curl -u $GITHUBUSER 'https://api.github.com/users/cptaffe/repos' | grep "\"name\": " | sed -r 's/"name": "(.*)"/\1/' | sed s/,//)"
 
 for name in $REPONAMES
 do
-	git clone "http://github.com/cptaffe/$name"
+	git clone "http://github.com/$GITHUBUSER/$name"
 done
